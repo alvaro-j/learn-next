eb3f5d8289dcc2f8da34bc5e30f8cf5f77a7adc7
alvaro-j <alvarojaime360@gmail.com>
2022-07-30 20:33:37 -0300

    import styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import styles from "./layout.module.css"
+
 const Layout = ({children}) => {
   return (
-    <div>{children}</div>
+    <div className={styles.container} >{children}</div>
   )
 }
 
