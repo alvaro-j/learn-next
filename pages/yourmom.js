b3d47c3804ceca5769e2b5dabb8ed32940e3b091
alvaro-j <alvarojaime360@gmail.com>
2022-07-30 14:46:02 -0300

    import Image component from next

--- pages/yourmom.js
+++ pages/yourmom.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function FirstPost() {
   const aaaaaaaa = "test";
@@ -6,6 +7,7 @@ export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
+      <Image src={"/images/img.jpg"} width={300} height={200} alt="rickroll" />
       <h2>
         <Link href="/">
           <a>Back to home</a>
