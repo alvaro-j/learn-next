e398b709450ee9998dd557d87f61872b0a6e3409
alvaro-j <alvarojaime360@gmail.com>
2022-07-30 15:14:53 -0300

    add Head component from next

--- pages/yourmom.js
+++ pages/yourmom.js
@@ -1,11 +1,15 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import Head from 'next/head';
 
 export default function FirstPost() {
   const aaaaaaaa = "test";
   console.log(aaaaaaaa);
   return (
     <>
+    <Head>
+      <title>your mom</title>
+    </Head>
       <h1>First Post</h1>
       <Image src={"/images/img.jpg"} width={300} height={200} alt="rickroll" />
       <h2>
