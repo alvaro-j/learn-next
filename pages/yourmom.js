9a67b9b88f07544f456d84d7bb234bcda5464a8d
alvaro-j <alvarojaime360@gmail.com>
2022-07-30 19:01:14 -0300

    add comments and Script component

--- pages/yourmom.js
+++ pages/yourmom.js
@@ -1,7 +1,7 @@
-import Link from "next/link";
-import Image from "next/image";
-import Head from "next/head";
-import Script from "next/script";
+import Link from "next/link"; // send to other page
+import Image from "next/image"; // set width and height for imgs with no effort
+import Head from "next/head"; // to config the <head> for each route/page
+import Script from "next/script"; // third-party scripts
 
 const yourmom = () => {
 	return (
@@ -9,6 +9,11 @@ const yourmom = () => {
 			<Head>
 				<title>your mom</title>
 			</Head>
+			<Script
+				src="https://connect.facebook.net/en_US/sdk.js"
+				strategy="lazyOnload"
+				onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)}
+			/>
 			<h1>First Post</h1>
 			<Image src={"/images/img.jpg"} width={300} height={200} alt="rickroll" />
 			<h2>
