765b4c4d76d7a887198ebfd82c8142c71a936d89
alvaro-j <alvarojaime360@gmail.com>
2022-07-30 18:50:32 -0300

    import Script from next and change to arrow func

--- pages/yourmom.js
+++ pages/yourmom.js
@@ -1,22 +1,23 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import Head from 'next/head';
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script";
 
-export default function FirstPost() {
-  const aaaaaaaa = "test";
-  console.log(aaaaaaaa);
-  return (
-    <>
-    <Head>
-      <title>your mom</title>
-    </Head>
-      <h1>First Post</h1>
-      <Image src={"/images/img.jpg"} width={300} height={200} alt="rickroll" />
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </>
-  );
-}
+const yourmom = () => {
+	return (
+		<>
+			<Head>
+				<title>your mom</title>
+			</Head>
+			<h1>First Post</h1>
+			<Image src={"/images/img.jpg"} width={300} height={200} alt="rickroll" />
+			<h2>
+				<Link href="/">
+					<a>Back to home</a>
+				</Link>
+			</h2>
+		</>
+	);
+};
+
+export default yourmom;
